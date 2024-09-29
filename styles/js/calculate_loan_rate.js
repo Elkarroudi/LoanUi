e78c5fa4854b7611c8ed575090f335744b7bf02f
Elkarroudi <amineelkarroudi04@proton.me>
2024-09-29 21:55:10 +0100

    Finish Loan Information

--- styles/js/calculate_loan_rate.js
+++ styles/js/calculate_loan_rate.js
@@ -8,12 +8,49 @@
  * @returns {number} The monthly payment amount.
  */
 
+let proportionalRate = 12;
+function isValidNumber(value) { return !isNaN(value) && value > 0; }
 function calculateLoanRate(borrowedCapital, proportionalRate, monthlyPaymentsNumber) {
 
-    if (isNaN(borrowedCapital) || isNaN(proportionalRate) || isNaN(monthlyPaymentsNumber)) {
+    if (![borrowedCapital, proportionalRate, monthlyPaymentsNumber].every(isValidNumber)) {
         return 0;
     }
 
     let monthlyRate = (proportionalRate / 100) / 12;
     return (borrowedCapital * monthlyRate) / (1 - Math.pow((1 + monthlyRate), -monthlyPaymentsNumber));
 }
+
+
+
+// Loan Amount Input and Show Box
+let amount = document.querySelector("input[name='amount']");
+let amount_box = document.getElementById("amount_box");
+
+// Loan Duration Input and Show Box
+let duration = document.querySelector("input[name='duration']");
+let duration_box = document.getElementById("duration_box");
+
+// LOan Rate Input
+let loan_rate = document.querySelector("input[name='loan_rate']");
+
+amount.oninput = function () {
+    amount_box.innerText = amount.value + " DH";
+    calculateLoanRateInAction();
+}
+
+duration.oninput = function () {
+    duration_box.innerText = duration.value + " Month";
+    calculateLoanRateInAction();
+}
+
+function calculateLoanRateInAction() {
+    let rate = calculateLoanRate(amount.value, proportionalRate, duration.value);
+    loan_rate.value = rate.toFixed(2) + " DH";
+}
+
+function initialiseLoanRate() {
+    amount_box.innerText = amount.value + " DH";
+    duration_box.innerText = duration.value + " Month";
+    calculateLoanRateInAction();
+}
+initialiseLoanRate();
